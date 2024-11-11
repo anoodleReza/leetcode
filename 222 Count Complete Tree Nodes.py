dfc47912210bfd913d945728889c8134cfe4ad49
anoodleReza <reza.brilliansah77@gmail.com>
2024-11-11 23:40:34 +0000

    222. Count Complete Tree Nodes

---
+++ 222 Count Complete Tree Nodes.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# input = root of a complete binary tree
+# output = number of nodes in the tree
+# runs in O(n) time
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
