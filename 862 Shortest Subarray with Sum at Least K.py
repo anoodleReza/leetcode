1662652311af5e64c80bf1860ad5d11d4c1ca81d
anoodleReza <reza.brilliansah77@gmail.com>
2024-11-17 00:30:14 +0000

    862. Shortest Subarray with Sum at Least K

---
+++ 862 Shortest Subarray with Sum at Least K.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        n = len(nums) + 1
+        prefixSums = [0] * n
+
+        for i in range(1, n):
+            prefixSums[i] = prefixSums[i - 1] + nums[i - 1]
+
+        candidates = deque()
+        shortest = float("inf")
+
+        for i in range(n):
+            while candidates and prefixSums[i] - prefixSums[candidates[0]] >= k:
+                shortest = min(shortest, i - candidates.popleft())
+            while candidates and prefixSums[i] <= prefixSums[candidates[-1]]:
+                candidates.pop()
+            candidates.append(i)
+
+        return shortest if shortest != float("inf") else -1
+
+
+
